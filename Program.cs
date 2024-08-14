5de921e969e519ed7e045cb1aac6d8539631991a
Bruno Araujo <bruno.cadoma@gmail.com>
2024-08-14 02:53:47 -0300

    feat: utilizando abstração, polimorfismo e herança

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone sp = new Iphone("15991003882", "13", "001970-01-010000-0", 32);
+sp.InstalarAplicativo("WhatsApp");
+sp.Ligar();
+Console.WriteLine(sp.Memoria);
+
+Smartphone sp2 = new Nokia("15991003885", "1", "001810-01-020000-0", 8);
+sp2.InstalarAplicativo("Instagram");
+sp2.Ligar();
+sp2.ReceberLigacao();
+
